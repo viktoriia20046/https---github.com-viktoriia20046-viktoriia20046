1ea73272cdd2abf6b2dd0716fabe3b55048705d7
viktoriia20046 <154357828+viktoriia20046@users.noreply.github.com>
2024-01-31 20:46:58 +0200

    update main.my and task 3

--- main.py
+++ main.py
@@ -51,22 +51,19 @@ def get_numbers_ticket(minimum, maximum, quantity):
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
 
-#Завдання 3
-
-import re
-
 def normalize_phone(phone_number):
-    # Видаляємо всі символи, крім цифр та '+'
-    digits_and_plus = re.sub(r"[^0-9+]", "", phone_number)
+    # Видаляємо всі символи, окрім цифр та '+'
+    digits_and_plus = "".join(filter(str.isdigit, phone_number))
 
+# Завдання 3
     # Перевіряємо, чи номер починається з '+', і виправляємо префікс згідно з вказівками
     if digits_and_plus.startswith("+"):
         normalized_number = digits_and_plus
-    elif digits_and_plus.startswith("380"):
+    elif digits_and_plus.startswith("380") and len(digits_and_plus) == 12:
         normalized_number = "+" + digits_and_plus
-    elif digits_and_plus.startswith("80"):
+    elif digits_and_plus.startswith("80") and len(digits_and_plus) == 11:
         normalized_number = "+3" + digits_and_plus[1:]
-    elif digits_and_plus.startswith("0"):
+    elif digits_and_plus.startswith("0") and len(digits_and_plus) == 10:
         normalized_number = "+38" + digits_and_plus[1:]
     else:
         normalized_number = "+" + digits_and_plus
@@ -75,8 +72,8 @@ def normalize_phone(phone_number):
 
 # Приклад використання
 raw_numbers = [
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
+    "067\t123 4567",
+    "(095) 234-5678\n",
     "+380 44 123 4567",
     "380501234567",
     "    +38(050)123-32-34",
@@ -88,7 +85,6 @@ raw_numbers = [
 
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
-
 
 
 #4 завдання 
